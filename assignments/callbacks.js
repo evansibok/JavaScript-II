1db3f8cfde581dc9863fd56c4d67751e272c403f
Evans Ibok <evansibok@gmail.com>
2019-08-20 13:13:38 +0100

    updating changes in callbacks.js

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -38,37 +38,33 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
   // "this Pencil is worth a million dollars!"
 */
 
-
+let result = []
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
   return cb(arr.length);
 }
-console.log(items, getLength);
+
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
   return cb(arr.length - 1);
 }
-// console.log(last);
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
-  // return cb(sumNumbs(x + y));
+  return cb(x + y);
 }
-// console.log(sumNums);
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
-  // return cb(multiplyNums(x * y));
+  return cb(x * y);
 }
-console.log(multiplyNums);
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
-  // return cb(item in list);
+  return cb(item in list);
 }
-console.log(contains);
 
 // QUESTION BY EVANS IBOK
 // So the above code is my solution but I found the code below online as a solution,
