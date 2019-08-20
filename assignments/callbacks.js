5457d8476462cc05279f1684c7b1e3e756c232ac
Evans Ibok <evansibok@gmail.com>
2019-08-20 12:17:17 +0100

    finished challenge 3 on callback.js

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -47,13 +47,15 @@ console.log(getLength);
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
-  return cb(arr.length - 1)
+  return cb(arr.length - 1);
 }
 console.log(last);
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(sumNumbs(x, y));
 }
+console.log(sumNums);
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
