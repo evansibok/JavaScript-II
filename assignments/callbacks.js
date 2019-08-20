d817bc90ee3ba4c8bae1c99144d1ad7d2e140e9d
Evans Ibok <evansibok@gmail.com>
2019-08-20 12:39:21 +0100

    corrected challenge 1 code in callback.js

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -43,30 +43,30 @@ function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
   return cb(arr.length);
 }
-console.log(getLength);
+console.log(items, getLength);
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
   return cb(arr.length - 1);
 }
-console.log(last);
+// console.log(last);
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
-  return cb(sumNumbs(x + y));
+  // return cb(sumNumbs(x + y));
 }
-console.log(sumNums);
+// console.log(sumNums);
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
-  return cb(multiplyNums(x * y));
+  // return cb(multiplyNums(x * y));
 }
 console.log(multiplyNums);
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
-  return cb(item in list);
+  // return cb(item in list);
 }
 console.log(contains);
 
