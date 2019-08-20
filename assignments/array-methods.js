3bae12a37be384246e4a46e102d54e37690273fb
evansibok <evansibok@gmail.com>
2019-08-20 17:39:28 +0100

    completed challenge 2 in callbacks.js

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -58,11 +58,15 @@ const runners = [
 // ==== Challenge 1: Use .forEach() ====
 // The event director needs both the first and last names of each runner for their running bibs. Combine both the first and last names and populate a new array called `fullNames`. This array will contain just strings.
 let fullNames = [];
+// fullNames = fullNames.forEach(() => `${first_name} ${last_name}`);
 console.log(fullNames);
 
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runners' first names in uppercase because the director BECAME DRUNK WITH POWER. Populate an array called `firstNamesAllCaps`. This array will contain just strings.
 let firstNamesAllCaps = [];
+firstNamesAllCaps = firstNamesAllCaps.map((cap_name) => {
+  return cap_name.first_name.toUpperCase();
+});
 console.log(firstNamesAllCaps);
 
 // ==== Challenge 3: Use .filter() ====
--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,6 +1,6 @@
 // Create a higher order function and invoke the callback function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
 
-const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
+const items = ["Pencil", "Notebook", "yo-yo", "Gum"];
 
 /* 
 
@@ -38,17 +38,22 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
   // "this Pencil is worth a million dollars!"
 */
 
-let result = []
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
   return cb(arr.length);
 }
-
+getLength(items, (a) => {
+  console.log(a);
+});
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
-  return cb(arr.length - 1);
+  return cb(arr[arr.length - 1]);
 }
+
+last(items, (a) => {
+	console.log(a);
+  });
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
@@ -71,7 +76,7 @@ function contains(item, list, cb) {
 // I'm confused how the callback in the return statement is calling either true or false,
 // is that allowed and what are the consequences?
 function contains(item, list, cb) {
-  if (list.includes(item)){
+  if (list.includes(item)) {
     return cb(true);
   }
   return cb(false);
